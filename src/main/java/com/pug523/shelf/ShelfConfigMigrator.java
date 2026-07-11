5928aef94fce5e7b628ba8f771fb1829c00312bc
pugur <pugurmc@gmail.com>
2026-07-11 21:45:12 +0900

    refactor: more typical typo case

--- src/main/java/com/pug523/shelf/ShelfConfigMigrator.java
+++ src/main/java/com/pug523/shelf/ShelfConfigMigrator.java
@@ -5,7 +5,7 @@ import com.pug523.shelf.config.Migrator;
 public class ShelfConfigMigrator {
     // @formatter:off
     public static final Migrator migrator = new Migrator().addRenameRule("someOldName", "brandNewName")
-            .addRenameRule("configuWizuTypo", "configWithTypo")
+            .addRenameRule("configuWizuTypo", "configWithoutTypo")
             .addRenameRule("oldCaseStyle", "new_case_style");
     // @formatter:on
 }
