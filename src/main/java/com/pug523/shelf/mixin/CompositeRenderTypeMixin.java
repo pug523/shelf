998ad570b86bfcfe2bee062446f47abecd45af0a
pugur <pugurmc@gmail.com>
2026-07-16 22:31:23 +0900

    build: remove render pipeline / pass as it does not exist in 1.21.3 or below

--- src/main/java/com/pug523/shelf/mixin/CompositeRenderTypeMixin.java
+++ src/main/java/com/pug523/shelf/mixin/CompositeRenderTypeMixin.java
@@ -2,12 +2,11 @@ package com.pug523.shelf.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 
+// @formatter:off
 //#if MC <= 12105
 //$$ import net.minecraft.client.renderer.RenderType;
 //$$ import com.pug523.shelf.gui.renderer.RenderPipelines;
 //$$ import com.pug523.shelf.gui.renderer.shader.UniformRegistry;
-//$$ import com.mojang.blaze3d.pipeline.RenderPipeline;
-//$$ import com.mojang.blaze3d.systems.RenderPass;
 //$$ import com.mojang.blaze3d.vertex.MeshData;
 //$$ import com.llamalad7.mixinextras.sugar.Local;
 //$$ import org.spongepowered.asm.mixin.injection.At;
@@ -15,9 +14,14 @@ import org.spongepowered.asm.mixin.Mixin;
 //$$ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 //$$ import org.spongepowered.asm.mixin.Final;
 //$$ import org.spongepowered.asm.mixin.Shadow;
+    //#if MC >= 12104
+    //$$ import com.mojang.blaze3d.pipeline.RenderPipeline;
+    //$$ import com.mojang.blaze3d.systems.RenderPass;
+    //#endif
 //#else
 import com.pug523.shelf.Shelf;
 //#endif
+// @formatter:on
 
 //#if MC <= 12105
 //$$ @Mixin(targets = "net.minecraft.client.renderer.RenderType$CompositeRenderType")
@@ -25,8 +29,12 @@ import com.pug523.shelf.Shelf;
 @Mixin(Shelf.class)
 //#endif
 public class CompositeRenderTypeMixin {
+    // @formatter:off
     //#if MC <= 12105
-    //$$ @Shadow @Final private RenderPipeline renderPipeline;
+
+        //#if MC >= 12104
+        //$$ @Shadow @Final private RenderPipeline renderPipeline;
+        //#endif
 
     //$$ @Inject(
     //$$         method = "draw(Lnet/mojang/blaze3d/vertex/MeshData;)V",
@@ -41,4 +49,5 @@ public class CompositeRenderTypeMixin {
     //$$     }
     //$$ }
     //#endif
+    // @formatter:on
 }
